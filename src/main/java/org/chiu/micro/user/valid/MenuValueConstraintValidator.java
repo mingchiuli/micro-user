ca5bcb2ef2a945de6edcdc2589cc541c70cdce6d
mingchiuli <mingchiuli@163.com>
2024-07-25 22:18:51 +0800

    update

--- src/main/java/org/chiu/micro/user/valid/MenuValueConstraintValidator.java
+++ src/main/java/org/chiu/micro/user/valid/MenuValueConstraintValidator.java
@@ -12,10 +12,13 @@ import org.chiu.micro.user.req.MenuEntityReq;
 import org.springframework.util.StringUtils;
 
 import java.util.Objects;
+import java.util.regex.Pattern;
 
 import static org.chiu.micro.user.lang.ExceptionMessage.*;
 
 public class MenuValueConstraintValidator implements ConstraintValidator<MenuValue, MenuEntityReq> {
+
+    private static final Pattern pattern = Pattern.compile("^(https?|ftp|file)://[-a-zA-Z0-9+&@#/%?=~_|!:,.;]*[-a-zA-Z0-9+&@#/%=~_|]");
 
     @Override
     public boolean isValid(MenuEntityReq menu, ConstraintValidatorContext context) {
@@ -32,6 +35,10 @@ public class MenuValueConstraintValidator implements ConstraintValidator<MenuVal
         }
 
         if (Objects.isNull(menu.getType())) {
+            return false;
+        }
+
+        if (!pattern.matcher(menu.getUrl()).matches()) {
             return false;
         }
 
